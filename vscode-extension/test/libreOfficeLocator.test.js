0c6c905a73a68be551c84cc8cd78bc26256c8187
Haruka Karakawa <rinnrenn424@yahoo.co.jp>
2026-07-23 16:32:44 +0900

    Fix LibreOffice locator test across platforms

--- vscode-extension/test/libreOfficeLocator.test.js
+++ vscode-extension/test/libreOfficeLocator.test.js
@@ -1,13 +1,15 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
+import path from 'node:path';
 import { getPathCandidates, findLibreOffice } from '../src/preview/libreOfficeLocator.js';
 
 test('PATH candidates use container libreoffice and soffice only', () => {
-  assert.deepEqual(getPathCandidates(['/usr/bin', '/opt/bin'].join(':')), [
-    '/usr/bin/libreoffice',
-    '/usr/bin/soffice',
-    '/opt/bin/libreoffice',
-    '/opt/bin/soffice'
+  const dirs = ['usr-bin', 'opt-bin'];
+  assert.deepEqual(getPathCandidates(dirs.join(path.delimiter)), [
+    path.join('usr-bin', 'libreoffice'),
+    path.join('usr-bin', 'soffice'),
+    path.join('opt-bin', 'libreoffice'),
+    path.join('opt-bin', 'soffice')
   ]);
 });
 
